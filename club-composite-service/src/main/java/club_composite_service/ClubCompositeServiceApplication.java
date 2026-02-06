3c95d5251cd438d9a1a71ad9f1a80d3e8104caaf
milospejic <milospejic2001@gmail.com>
2026-02-06 22:45:19 +0100

    Removed api-gateway, eureka-server and conig-server projects. Added tracing dependencies to microservices. Removed docker-compose file. Updated workflow file.

--- club-composite-service/src/main/java/club_composite_service/ClubCompositeServiceApplication.java
+++ club-composite-service/src/main/java/club_composite_service/ClubCompositeServiceApplication.java
@@ -2,14 +2,11 @@ package club_composite_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @ComponentScan(basePackages = {"club_composite_service", "util"})
 public class ClubCompositeServiceApplication {
 
--- postgres-init/create-databases.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE user_db;
-CREATE DATABASE club_db;
-CREATE DATABASE reservation_db;
-CREATE DATABASE review_db;
-CREATE DATABASE notification_db;
